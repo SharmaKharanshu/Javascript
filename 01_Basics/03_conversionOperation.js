5f9c286c8e4c77984e9ea0f30132acf5a0976220
Kharanshu <138291947+SharmaKharanshu@users.noreply.github.com>
2023-10-24 18:58:58 +0000

    conversion of datatype

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score="33"
+typeof(score) //string
+let valueInNumber=Number(score);
+console.log(valueInNumber);
+
+//case 2
+let score1="33abc"
+typeof(score1) //string
+let valueInNumber1=Number(score1);
+console.log(valueInNumber1);   //Nan [Not a number] special type (IMPORTANT)
+
+//case 3 
+let score2=null
+typeof(score2) //object
+let valueInNumber2=Number(score2);
+console.log(valueInNumber2);   //Gives 0 in output
+
+//case 1 -> "33" = 33
+//case 2 -> "33abc"= Nan
+//case 3 -> "null" = 0
