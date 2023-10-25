d8027e301124c4a8b99394a1d016cb23f721e875
Kharanshu <138291947+SharmaKharanshu@users.noreply.github.com>
2023-10-25 08:47:47 +0000

    date & time

---
+++ 01_Basics/07_date_and_time.js
@@ -0,0 +1,7 @@
+// TC39 working on temporal a new date/time API
+// trying to make date api global like Math
+
+let myDate=new Date(); // created date object, myDate is ojb ect
+console.log(myDate);  // it gives 2023-10-25T08:41:25.663Z
+
+console.log(myDate.toString()); // gives -> Wed Oct 25 2023 08:42:38 GMT+0000 (Coordinated Universal Time)
